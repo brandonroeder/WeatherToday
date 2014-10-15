6ed1ebc6059c429449b45d4ce09bc0042682506e
Brandon Roeder <brandon@Brandons-MacBook-Pro.local>
2014-10-15 01:20:39 -0500

    Added caching and auto refresh

---
+++ WeatherToday/AppDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  AppDelegate.swift
+//  WeatherToday
+//
+//  Created by Brandon Roeder on 9/30/14.
+//  Copyright (c) 2014 Brandon Roeder. All rights reserved.
+//
+
+import Foundation
---
+++ WeatherToday/ViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ViewController.swift
+//  WeatherToday
+//
+//  Created by Brandon Roeder on 9/29/14.
+//  Copyright (c) 2014 Brandon Roeder. All rights reserved.
+//
+
+import Foundation

225db413e8437a69f42c5c8f6410a3a7b47bb2b4
Brandon Roeder <brandonsroeder@gmail.com>
2015-04-14 16:27:27 -0500

    Added Watch Target

---
+++ WeatherToday WatchKit Extension/InterfaceController.m
@@ -0,0 +1,43 @@
+//
+//  InterfaceController.m
+//  WeatherToday WatchKit Extension
+//
+//  Created by Brandon Roeder on 11/18/14.
+//  Copyright (c) 2014 Brandon Roeder. All rights reserved.
+//
+
+#import "InterfaceController.h"
+
+
+@interface InterfaceController()
+
+@end
+
+
+@implementation InterfaceController
+
+- (instancetype)initWithContext:(id)context {
+    self = [super initWithContext:context];
+    if (self){
+        // Initialize variables here.
+        // Configure interface objects here.
+        NSLog(@"%@ initWithContext", self);
+        
+    }
+    return self;
+}
+
+- (void)willActivate {
+    // This method is called when watch view controller is about to be visible to user
+    NSLog(@"%@ will activate", self);
+}
+
+- (void)didDeactivate {
+    // This method is called when watch view controller is no longer visible
+    NSLog(@"%@ did deactivate", self);
+}
+
+@end
+
+
+
---
+++ WeatherToday WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  WeatherToday WatchKit Extension
+//
+//  Created by Brandon Roeder on 11/18/14.
+//  Copyright (c) 2014 Brandon Roeder. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
